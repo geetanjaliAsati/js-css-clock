a996fd41299fa0fde47b05b69d229edf15ad7f7f
geetanjaliAsati <104821909+geetanjaliAsati@users.noreply.github.com>
2022-07-06 23:07:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,14 +8,14 @@ function setTime(){
   const minutes = now.getMinutes();
   const hours = now.getHours();
 
-  const secDegrees = ((seconds/60)360) + 90;
-  secondHand.style.transform = rotate(`${secDegrees}deg`)
+  const secDegrees = ((seconds/60)*360) + 90;
+  secondHand.style.transform = `rotate(${secDegrees}deg)`
 
-  const minDegrees = (minutes/60360)+90 + (seconds/60)6;
-  minHand.style.transform = rotate(`${minDegrees}deg`)
+  const minDegrees = (minutes/60*360)+90 + (seconds/60)*6;
+  minHand.style.transform = `rotate(${minDegrees}deg)`
 
-  const hourDegrees = (hours/12360)+90 + (minutes/60)*30;
-  hourHand.style.transform = rotate(`${hourDegrees}deg`)
+  const hourDegrees = (hours/12*360)+90 + (minutes/60)*30;
+  hourHand.style.transform = `rotate(${hourDegrees}deg)`
 
 }
 setInterval(setTime, 1000);
